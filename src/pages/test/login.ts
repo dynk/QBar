ea71096cd1a4996e43d588b98c2d86842ee82705
dynk <rsalvador1@luc.edu>
2016-12-21 08:43:22 -0200

    cleaning up the code

--- src/pages/test/login.ts
+++ src/pages/test/login.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import 'rxjs/add/operator/take';
-
-
 
 
 @Component({
@@ -10,13 +7,7 @@ import 'rxjs/add/operator/take';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  // Test RDNS
-  // books: FirebaseListObservable<any>;
-  booksOnce;
-  constructor(public navCtrl: NavController) {
-    // this.books = angFire.database.list('/Books');
-
-  }
+  constructor(public navCtrl: NavController) {  }
 
   ionViewDidLoad() {
     console.log('LoginPage Page Open');
--- src/providers/databaseprovider.ts
+++ src/providers/databaseprovider.ts
@@ -1,36 +1,11 @@
 import { Injectable } from '@angular/core';
-import { AngularFire } from 'angularfire2';
-import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/map';
 import { Config } from '../config/config'
 
 @Injectable()
 export class Databaseprovider {
-  // config;
-  constructor(public angFire: AngularFire) {
-    // this.config = Config;
-    // console.log(this.config);
-  }
 
-  getData(){
-    return this.getFirebaseData();
-  }
-
-
-  getFirebaseData(){
-
-    // return this.angFire.database.list('/Books').take(1);
-    return this.angFire.database.list('/Books').take(1).map(data =>{
-      console.log(data);
-      return data;
-    });
-    // return firebase.database().ref('/Books' ).once('value').then(function(snapshot) {
-    //   console.log(snapshot.val());
-    //   return snapshot.val();
-    // });
-
-  }
-
+  constructor() {}
 
 
 
